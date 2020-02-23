def4d93b7830318c817c141ec090cbb3da218dc2
woohm402 <39977696+woohm402@users.noreply.github.com>
2020-02-23 21:50:09 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        GameRun gameRun = new GameRun(Properties.n,Properties.block_size, Properties.space);
+    }
+}
---
+++ Properties.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Properties {
+    final static int n=4;
+    final static int block_size=100;
+    final static int space=10;
+    final static int topSpace=100;
+    final static int bottomSpace=30;
+    final static Color blockColor = new Color(240,240,240);
+    final static Color backgroundColor = new Color(255,200,200);
+
+    final static Font endFont = new Font("SansSerif",Font.BOLD, 50);
+    final static Font scoreFont = new Font("SansSerif",Font.BOLD, 20);
+    final static Font blockFont = new Font("SansSerif",Font.BOLD, 35);
+}
