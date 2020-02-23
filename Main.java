5416278a9b73f6eb9e7c448de0a5cff0bc149c3f
woohm402 <39977696+woohm402@users.noreply.github.com>
2020-02-23 21:52:15 +0900

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        GameRun gameRun = new GameRun(Properties.n,Properties.block_size, Properties.space);
-    }
-}
